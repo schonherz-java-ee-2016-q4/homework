c160bc45170262e9d2e4ae91a1a837fa570cc47c
David Pinter <djazz90@gmail.com>
2016-11-15 21:42:31 +0100

    MultiMap homework implemented

---
+++ pinter-david/1/src/hu/homework/davidp/DefaultMultiMap.java
@@ -0,0 +1,74 @@
+package hu.homework.davidp;
+
+import java.util.*;
+
+public class DefaultMultiMap<K, V> implements MultiMap<K, V> {
+    private Map<K, ArrayList<V>> data;
+
+    public DefaultMultiMap(){
+        data = new HashMap<>();
+    }
+
+    public DefaultMultiMap(Map<K, ArrayList<V>> data) {
+        this.data = data;
+    }
+
+    @Override
+    public void clear() {
+        data.clear();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return data.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return data.containsValue(value);
+    }
+
+    @Override
+    public Collection<V> get(Object key) {
+        return data.get(key);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    @Override
+    public V put(K key, V value) {
+        if (!data.containsKey(key)) {
+            data.put(key, new ArrayList<>());
+        }
+        data.get(key).add(value);
+        return value;
+    }
+
+    @Override
+    public Collection<V> remove(Object key) {
+        return data.remove(key);
+    }
+
+    @Override
+    public Object remove(Object key, Object value) {
+        boolean isEmptyDataValue = data.containsKey(key) && data.get(key).size() == 0;
+
+        if (!data.containsKey(key) || isEmptyDataValue || !data.get(key).contains(value)) {
+            return null;
+        }
+
+        return data.get(key).remove(data.get(key).indexOf(value));
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+
+    public Set<Map.Entry<K, ArrayList<V>>> entrySet() {
+        return data.entrySet();
+    }
+}
---
+++ pinter-david/1/src/hu/homework/davidp/Main.java
@@ -0,0 +1,25 @@
+package hu.homework.davidp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DefaultMultiMap<String, String> testMap = new DefaultMultiMap<>();
+
+        testMap.put("Batman", "batknife");
+        testMap.put("Batman", "batmobile");
+        testMap.put("Robin", "65");
+        testMap.put("Snoop Dogg", "420");
+
+        System.out.println("testmap at its full glory: ");
+        testMap.entrySet().forEach(System.out::println);
+
+        testMap.remove("Robin");
+        System.out.println("\nafter robin removed from the testmap: ");
+        testMap.entrySet().forEach(System.out::println);
+
+        System.out.println("\nlet's take the batknife from Batman: ");
+        testMap.remove("Batman","batknife");
+        testMap.entrySet().forEach(System.out::println);
+
+    }
+}
