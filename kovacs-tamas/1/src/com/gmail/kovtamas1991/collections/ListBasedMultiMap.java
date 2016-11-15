a599a4791bae6a1bb68a6d4f2e94d52c6ae85e00
Joey <kovtamas1991@gmail.com>
2016-11-15 16:23:56 +0100

    Adopting to changed homework requirements. Renamed old multi map, created MultiMap interface and a class with generated implementing methods.

---
+++ kovacs-tamas/1/src/com/gmail/kovtamas1991/collections/ListBasedMultiMap.java
@@ -0,0 +1,63 @@
+package com.gmail.kovtamas1991.collections;
+
+import java.util.Collection;
+
+public class ListBasedMultiMap<K, V> implements MultiMap<K, V> {
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Collection<V> get(Object key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<V> remove(Object key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key, Object value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+
+
+}
