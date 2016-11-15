b2a0264986a66000abf01f2a2d35a5ae3f921e39
Joey <kovtamas1991@gmail.com>
2016-11-15 16:35:53 +0100

    Copied structure and implementations from my util-multimap that can be used in the new version.

--- kovacs-tamas/1/src/com/gmail/kovtamas1991/collections/ListBasedMultiMap.java
+++ kovacs-tamas/1/src/com/gmail/kovtamas1991/collections/ListBasedMultiMap.java
@@ -1,24 +1,48 @@
 package com.gmail.kovtamas1991.collections;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ListBasedMultiMap<K, V> implements MultiMap<K, V> {
 
+    private List<K> keys;
+    private List<LinkedList<V>> values;
+
+    public ListBasedMultiMap() {
+        keys = new LinkedList<>();
+        values = new ArrayList<>();
+    }
+
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-
+        keys.clear();
+        values.clear();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        // TODO Auto-generated method stub
-        return false;
+        if (key == null) {
+            return false;
+        }
+        return keys.contains(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        // TODO Auto-generated method stub
+        if (value == null) {
+            return false;
+        }
+
+        for (List<V> currentValueList : values) {
+            for (V currentValue : currentValueList) {
+                if (currentValue.equals(value)) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
@@ -30,14 +54,29 @@ public class ListBasedMultiMap<K, V> implements MultiMap<K, V> {
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
+        return keys.isEmpty();
     }
 
     @Override
     public V put(K key, V value) {
-        // TODO Auto-generated method stub
-        return null;
+        if (key == null || value == null) {
+            throw new IllegalArgumentException("Nor the key or value are allowed to be null!");
+        }
+
+        int keyIndex = keys.indexOf(key);
+        // if map already contains key
+        if (isValidKeyIndex(keyIndex)) {
+            LinkedList<V> existingValues = values.get(keyIndex);
+            V prevValue = existingValues.getLast();
+            existingValues.add(value);
+            return prevValue;
+        } else {
+            keys.add(key);
+            LinkedList<V> valueList = new LinkedList<>();
+            valueList.add(value);
+            values.add(valueList);
+            return null;
+        }
     }
 
     @Override
@@ -54,10 +93,11 @@ public class ListBasedMultiMap<K, V> implements MultiMap<K, V> {
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+        return keys.size();
     }
 
-
+    private boolean isValidKeyIndex(int keyIndex) {
+        return keyIndex >= 0 && keyIndex < keys.size();
+    }
 
 }
