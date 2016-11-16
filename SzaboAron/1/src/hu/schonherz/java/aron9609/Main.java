41bbac4d197d7b36745fb39f8e00bd32838ec296
aron9609 <aron9609@gmail.com>
2016-11-16 09:33:19 +0100

    Test of the MultiMapNode class

--- SzaboAron/1/src/hu/schonherz/java/aron9609/Main.java
+++ SzaboAron/1/src/hu/schonherz/java/aron9609/Main.java
@@ -1,5 +1,8 @@
 package hu.schonherz.java.aron9609;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import hu.schonherz.java.aron9609.multimap.MyMultiMap;
 
 public class Main {
@@ -30,9 +33,24 @@ public class Main {
 
 		System.out.println("Is TestMap empty? " + testMultiMap.isEmpty());
 
-		testMultiMap.clear();
+		// testMultiMap.clear();
 
 		System.out.println("Is TestMap empty after the clear method call? " + testMultiMap.isEmpty());
 
+		// Testing the MultiMapNode class
+
+		MyMultiMap.MultiMapNode<Integer, String> firstNode = new MyMultiMap.MultiMapNode<>(1, "test1.3");
+		MyMultiMap.MultiMapNode<Integer, String> secondNode = new MyMultiMap.MultiMapNode<>(2, "test2.4");
+		List<String> nodeValues = new ArrayList<>();
+		nodeValues.add("tst4");
+		nodeValues.add("tst4.1");
+		nodeValues.add("tst4.2");
+		nodeValues.add("tst4.3");
+		nodeValues.add("tst4.4");
+		MyMultiMap.MultiMapNode<Integer, String> thirdNode = new MyMultiMap.MultiMapNode<>(4, nodeValues);
+		testMultiMap.put(firstNode);
+		testMultiMap.put(secondNode);
+		testMultiMap.put(thirdNode);
+		System.out.println("Result of getting values of key 4 " + testMultiMap.get(4));
 	}
 }
