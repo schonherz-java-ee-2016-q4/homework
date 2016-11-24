3af8861108e75678d5007ed9ac893d7542c102b5
Joey <kovtamas1991@gmail.com>
2016-11-24 17:59:44 +0100

    Created package for the new classes and interfaces, created FileLoader interface and it's implementation, also imported the MultiMap for later use.

--- kovacs-tamas/2/src/hu/schonherz/java/training/main/Main.java
+++ kovacs-tamas/2/src/hu/schonherz/java/training/main/Main.java
@@ -1,6 +1,43 @@
 package hu.schonherz.java.training.main;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import hu.schonherz.java.training.domain.logic.SimpleFileLoader;
+import hu.schonherz.java.training.domain.logic.interfaces.FileLoader;
+
 public class Main {
+
     public static void main(final String... args) {
+        // 1. create Runnable
+        // 2. create thread and start it (daemon thread)
+        // 3. wait for user input
+        // 4. exit on receiving any user input
+
+        FileLoader fileLoader = new SimpleFileLoader();
+        BufferedReader reader = null;
+        try {
+            reader = fileLoader.getReaderForFile(FileLoader.Files.ADMIN);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (reader == null) {
+            System.err.println("Input stream is nul!");
+            return;
+        }
+
+        System.out.println("not null!");
+
+
+        try {
+            reader.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
+
 }
