1792858f00dda25a29e40e6ab0e3aecbc63410e6
Joey <kovtamas1991@gmail.com>
2016-11-25 11:27:41 +0100

    Added assert-free test class for the SimpleFileLoader class. It showed that the class works well, but I modified the wrong server.txt file. Reverted changes of that file.

--- kovacs-tamas/2/src/hu/schonherz/java/training/main/Main.java
+++ kovacs-tamas/2/src/hu/schonherz/java/training/main/Main.java
@@ -1,13 +1,5 @@
 package hu.schonherz.java.training.main;
 
-import com.gmail.kovtamas1991.collections.MultiMap;
-
-import hu.schonherz.java.training.domain.logic.FileParseException;
-import hu.schonherz.java.training.domain.logic.SimpleFileParser;
-import hu.schonherz.java.training.domain.logic.interfaces.FileParser;
-import hu.schonherz.java.training.domain.people.Employee;
-import hu.schonherz.java.training.domain.server.Server;
-
 public class Main {
 
     public static void main(final String... args) {
@@ -15,16 +7,6 @@ public class Main {
         // 2. create thread and start it (daemon thread)
         // 3. wait for user input
         // 4. exit on receiving any user input
-
-        FileParser parser = new SimpleFileParser();
-        MultiMap<Server, Employee> map = null;
-        try {
-            map = parser.getStoppedServersAndTheirAdmins();
-        } catch (FileParseException fpe) {
-            System.err.println("Nem nyert!");
-        }
-
-        System.out.println(map.size());
 
     }
 
---
+++ kovacs-tamas/2/src/tests/FileLoaderTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import hu.schonherz.java.training.domain.logic.Files;
+import hu.schonherz.java.training.domain.logic.SimpleFileLoader;
+import hu.schonherz.java.training.domain.logic.interfaces.FileLoader;
+
+public class FileLoaderTest {
+
+    FileLoader fileLoader;
+
+    @Before
+    public void setUp() throws Exception {
+        fileLoader = new SimpleFileLoader();
+    }
+
+    @Test
+    public void testServerFileLoading() throws FileNotFoundException, IOException {
+        try (BufferedReader serReader = fileLoader.getReaderForFile(Files.SERVER)) {
+            System.out.println("----- Contens of server file -----");
+            String line;
+            while ((line = serReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+    }
+
+    @Test
+    public void testAdminFileLoading() throws FileNotFoundException, IOException {
+        try (BufferedReader adminReader = fileLoader.getReaderForFile(Files.ADMIN)) {
+            System.out.println("----- Contens of admin file -----");
+            String line;
+            while ((line = adminReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+    }
+
+}
