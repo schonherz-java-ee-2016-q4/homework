7f6a7210ef6a20ca98df4051e4c0d29293174946
kiradave <hundood@gmail.com>
2016-11-27 20:00:14 +0100

    use mymultimap

---
+++ KiralyDavid/2/src/hu/schonherz/java/training/mymultimap/MultiMap.java
@@ -0,0 +1,29 @@
+package mymultimap;
+
+import java.util.Collection;
+
+public interface MultiMap<K, V> {
+
+	public void clear();
+
+	public boolean containsKey(Object key);
+
+	boolean containsValue(Object value);
+
+	public Collection<V> get(Object key);
+
+	public boolean isEmpty();
+
+	public V put(K key, V value);
+
+	public Collection<V> remove(Object key);
+
+	public Object remove(Object key, Object value);
+
+	public int size();
+
+	public boolean equals(Object o);
+
+	public int hashCode();
+
+}
