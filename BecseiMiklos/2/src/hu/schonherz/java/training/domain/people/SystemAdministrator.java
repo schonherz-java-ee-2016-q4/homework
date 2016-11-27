3e93093a5264bc7677f6ab73ff4a29b8b8b8a73d
Becsei Miklós <bmikyy@gmail.com>
2016-11-27 19:45:31 +0100

    server reading added

--- BecseiMiklos/2/src/hu/schonherz/java/training/domain/people/SystemAdministrator.java
+++ BecseiMiklos/2/src/hu/schonherz/java/training/domain/people/SystemAdministrator.java
@@ -2,18 +2,24 @@ package hu.schonherz.java.training.domain.people;
 
 import java.util.List;
 
-import hu.schonherz.java.training.domain.server.Server;
-
 public class SystemAdministrator extends Employee {
+	
+	String name;
+	int ID;
+	List<Integer> servers;
+	
+    public SystemAdministrator(String name, int employeeID, List<Integer> servers) {
+    	this.name = name;
+    	this.ID = employeeID;
+        this.servers = servers;
+    }
     private static final long serialVersionUID = 2244059719140625986L;
 
-    private List<Server> servers;
-
-    public List<Server> getServers() {
+    public List<Integer> getServers() {
         return servers;
     }
 
-    public void setServers(final List<Server> servers) {
+    public void setServers(final List<Integer> servers) {
         this.servers = servers;
     }
 
