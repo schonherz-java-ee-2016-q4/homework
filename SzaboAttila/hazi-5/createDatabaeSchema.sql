65a899e9805cd2a7084f625cafdc6939a6d05e7d
szati64 <szati64@gmail.com>
2016-12-12 17:44:34 +0100

    homework-5 database creation file added

---
+++ SzaboAttila/hazi-5/createDatabaeSchema.sql
@@ -0,0 +1,66 @@
+CREATE SCHEMA IF NOT EXISTS blog_homework;
+
+SET SCHEMA 'blog_homework';
+
+CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+    email VARCHAR(250) NOT NULL,
+    dob DATE NOT NULL,
+    registered DATE NOT NULL,
+    phone VARCHAR(30) NOT NULL,
+    cell VARCHAR(30) NOT NULL,
+    gender VARCHAR(1) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS location (
+	user_id INTEGER NOT NULL,
+	street VARCHAR(250) NOT NULL,
+	city VARCHAR(150) NOT NULL,
+	state VARCHAR(100) NOT NULL,
+	postcode INTEGER NOT NULL,
+	CONSTRAINT location_user_fk
+        FOREIGN KEY (user_id)
+        REFERENCES users(id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS name (
+	user_id INTEGER NOT NULL,
+	title VARCHAR(5) NOT NULL,
+    first VARCHAR(100) NOT NULL,
+    last VARCHAR(100) NOT NULL,
+	CONSTRAINT name_user_fk
+        FOREIGN KEY (user_id)
+        REFERENCES users(id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS login (
+	user_id INTEGER NOT NULL,
+	
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    
+	CONSTRAINT name_user_fk
+        FOREIGN KEY (user_id)
+        REFERENCES users(id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS picture (
+	user_id INTEGER NOT NULL,
+	
+	large VARCHAR(255) NOT NULL,
+    medium VARCHAR(255) NOT NULL,
+    thumbnail VARCHAR(255) NOT NULL,
+	
+	CONSTRAINT name_user_fk
+        FOREIGN KEY (user_id)
+        REFERENCES users(id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
