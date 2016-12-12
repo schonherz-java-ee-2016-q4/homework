cf20eac4363e8bbe3750fefb5290f8ed0fffc678
Joey <kovtamas1991@gmail.com>
2016-12-12 23:18:03 +0100

    Started to make the registration form and the client side validation.

---
+++ kovacs-tamas/4/hwblog/web/src/main/java/hu/schonherz/java/training/kovtamas/web/servlet/RegistrationServlet.java
@@ -0,0 +1,9 @@
+package hu.schonherz.java.training.kovtamas.web.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/RegistrationServlet")
+public class RegistrationServlet extends HttpServlet {
+
+}
