46bc46dcffb8c5ecdf3fba76fc59b19aa63d3696
Joey <kovtamas1991@gmail.com>
2016-12-22 19:06:45 +0100

    Filtering users now works (from javascript). Added 'age' field to Dates class, added some css styling to the secure page.

--- kovacs-tamas/4-5/hwblog/service-api/src/main/java/hu/schonherz/java/training/kovtamas/serviceapi/user/vo/Dates.java
+++ kovacs-tamas/4-5/hwblog/service-api/src/main/java/hu/schonherz/java/training/kovtamas/serviceapi/user/vo/Dates.java
@@ -1,11 +1,15 @@
 package hu.schonherz.java.training.kovtamas.serviceapi.user.vo;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Date;
 
 public class Dates {
 
     private Date birth;
     private Date registration;
+    private int age;
 
     public Dates() {
     }
@@ -16,6 +20,9 @@ public class Dates {
 
     public void setBirth(Date birth) {
         this.birth = birth;
+
+        LocalDate birthDay = birth.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        this.age = Period.between(birthDay, LocalDate.now()).getYears();
     }
 
     public Date getRegistration() {
@@ -24,6 +31,10 @@ public class Dates {
 
     public void setRegistration(Date registration) {
         this.registration = registration;
+    }
+
+    public int getAge() {
+        return age;
     }
 
 }
