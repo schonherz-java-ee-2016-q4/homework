a6ddaa1e8cbf7cc8891df14bed7748f7a7ab208b
Pintér Dávid <djazz90@gmail.com>
2016-12-24 16:48:26 +0100

    Homework 6 initial commit, refactored some module names for more consistent convention. Data layer WIP.

---
+++ pinter-david/6/blog/blogapp-data/src/main/java/hu/schonherz/blog/data/dto/UserDTO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.schonherz.blog.data.dto;
+
+/**
+ *
+ * @author pintyo
+ */
+public class UserDTO {
+    
+}
