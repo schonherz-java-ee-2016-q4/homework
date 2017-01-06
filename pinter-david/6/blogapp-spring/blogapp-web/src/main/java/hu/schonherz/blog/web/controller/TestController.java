234131a9840f40934e057b7357d7ecf26e5d3b13
David Pinter <djazz90@gmail.com>
2017-01-06 01:42:44 +0100

    Spring web module working now as expected, moving on.

---
+++ pinter-david/6/blogapp-spring/blogapp-web/src/main/java/hu/schonherz/blog/web/controller/TestController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.schonherz.blog.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author pintyo
+ */
+@Controller
+public class TestController {
+    
+   @RequestMapping(value = "/", method = RequestMethod.GET)
+   public String index(ModelMap map) {
+       map.put("msg", "Hello Spring 4 Web MVC!");
+       return "index";
+   }
+}
