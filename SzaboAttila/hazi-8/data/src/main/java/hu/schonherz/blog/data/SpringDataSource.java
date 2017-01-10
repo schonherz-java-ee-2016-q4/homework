e03aa28857c72af916f634391cbd37bb17ee6494
szati64 <szati64@gmail.com>
2017-01-10 23:04:35 +0100

    homework 8 - jpa dies on wildfly

--- SzaboAttila/hazi-8/data/src/main/java/hu/schonherz/blog/data/SpringDataSource.java
+++ SzaboAttila/hazi-8/data/src/main/java/hu/schonherz/blog/data/SpringDataSource.java
@@ -1,6 +1,5 @@
 package hu.schonherz.blog.data;
 
-import org.postgresql.ds.PGPoolingDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
@@ -10,16 +9,10 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringDataSource {
     @Bean
-    public DataSource createDataSource() {
-        PGPoolingDataSource inst = new PGPoolingDataSource();
-        inst.setServerName("localhost");
-        inst.setDatabaseName("postgres");
-        inst.setPortNumber(5432);
-        inst.setUser("postgres");
-        inst.setPassword("pass");
-
-        inst.setInitialConnections(10);
-        inst.setMaxConnections(20);
-        return inst;
+    public DataSource getDataSource() {
+        final JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();
+        dsLookup.setResourceRef(true);
+        DataSource dataSource = dsLookup.getDataSource("java:jboss/jdbc/example");
+        return dataSource;
     }
 }
